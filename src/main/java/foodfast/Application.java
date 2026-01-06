8270e199ab3809a4b729c69373f8ef3ae678b869
Clement-Szewczyk <clement.szewczyk@gmail.com>
2026-01-06 10:25:41 +0100

    Application maj

--- src/main/java/foodfast/Application.java
+++ src/main/java/foodfast/Application.java
@@ -2,9 +2,13 @@ package foodfast;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Bienvennue chez FoodFast !");
-        for (int i = 0; i<args.length; i++){
-            System.out.println("Argument " + i + ": "+ args[i]);
+        System.out.println("Bienvenue chez FoodFast !");
+
+        if (args.length > 0) {
+            System.out.println("Arguments passés :");
+            for (String arg : args) {
+                System.out.println("  - " + arg);
+            }
         }
     }
 }
--- src/test/java/ApplicationTest.java
+++ src/test/java/ApplicationTest.java
@@ -4,44 +4,6 @@ import org.junit.jupiter.api.Test;
 public class ApplicationTest {
     @Test
     public void test(){
-        System.out.println("Test");
         Assertions.assertEquals(1, 1);
-
-    }
-
-
-    @Test
-    public void testDeliveryPlanner(){
-        Assertions.assertEquals("Fizz", foodfast.FoodFastUtils.deliveryPlanner(3));
-        Assertions.assertEquals("Buzz", foodfast.FoodFastUtils.deliveryPlanner(5));
-        Assertions.assertEquals("FizzBuzz", foodfast.FoodFastUtils.deliveryPlanner(15));
-        Assertions.assertEquals("7", foodfast.FoodFastUtils.deliveryPlanner(7));
-        Assertions.assertEquals("22", foodfast.FoodFastUtils.deliveryPlanner(22));
-        Assertions.assertEquals("Fizz", foodfast.FoodFastUtils.deliveryPlanner(9));
-        Assertions.assertEquals("Buzz", foodfast.FoodFastUtils.deliveryPlanner(20));
-        Assertions.assertEquals("FizzBuzz", foodfast.FoodFastUtils.deliveryPlanner(30));
-    }
-
-    @Test
-    public void testisLeapYear(){
-        Assertions.assertEquals(true, foodfast.FoodFastUtils.isLeapYear(2020));
-        Assertions.assertEquals(false, foodfast.FoodFastUtils.isLeapYear(2021));
-        Assertions.assertEquals(false, foodfast.FoodFastUtils.isLeapYear(2022));
-        Assertions.assertEquals(false, foodfast.FoodFastUtils.isLeapYear(2023));
-        Assertions.assertEquals(true, foodfast.FoodFastUtils.isLeapYear(2024));
-    }
-
-    @Test
-    public void testsumUpTo(){
-        Assertions.assertEquals(10, foodfast.FoodFastUtils.sumUpTo(4));
-        Assertions.assertEquals(15, foodfast.FoodFastUtils.sumUpTo(5));
-        Assertions.assertEquals(21, foodfast.FoodFastUtils.sumUpTo(6));
-    }
-
-    @Test
-    public void testAnonymize(){
-        Assertions.assertEquals("nob", foodfast.FoodFastUtils.anonymize("bon"));
-        Assertions.assertEquals("olleh", foodfast.FoodFastUtils.anonymize("hello"));
-        Assertions.assertEquals("tejorp", foodfast.FoodFastUtils.anonymize("projet"));
     }
 }
